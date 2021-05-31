6b87e77e2e65e35ec04a695fd89586b33b7359f5
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-05-31 17:17:57 +0100

    Performing token refresh in our REST API

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,8 @@
 import hmac
 
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token,
+                                jwt_required, get_jwt)
 from flask_restful import Resource, reqparse
 
 from models.user import UserModel
@@ -64,3 +66,12 @@ class UserLogin(Resource):
                    }, 200
 
         return {"message": "Invalid credentials"}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}, 200
+
